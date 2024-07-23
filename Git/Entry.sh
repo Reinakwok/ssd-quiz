fcf603df79c560c1fc3e1611b5fa54f40316e9f9
Reinakwok <reinakwok@gmail.com>
2024-07-23 15:47:53 +0800

    initial files

---
+++ Git/Entry.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Set Git user information
+git config --global user.name "Reina Kwok"
+git config --global user.email "2203286@sit.singaporetech.edu.sg"
+
+# Start the git-http-server on port 3000
+exec git-http-server -p 3000 /home/git
+
